ec1a5eb526ac89ac15f56eed358161f7e9da5f94
Meek Msaki <meek10x@gmail.com>
2025-05-12 01:53:34 -0500

    solved commutativity

---
+++ LeanExercises/exercise1.lean
@@ -0,0 +1,33 @@
+/- Prove the following identities, replacing the "sorry" placeholders with actual proofs. -/
+variable (p q r : Prop)
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := 
+  Iff.intro
+    (fun (h: p ∧ q) => ⟨h.right, h.left⟩)
+    (fun (h: q ∧ p) => ⟨h.right, h.left⟩)
+example : p ∨ q ↔ q ∨ p :=
+  Iff.intro
+    (fun (h: p ∨ q) => h.elim (fun hp => Or.inr hp) (fun hq => Or.inl hq))
+    (fun (h: q ∨ p) => h.elim (fun hp => Or.inr hp) (fun hq => Or.inl hq))
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ False ↔ p := sorry
+example : p ∧ False ↔ False := sorry
+example : (p → q) → (¬q → ¬p) := sorry
